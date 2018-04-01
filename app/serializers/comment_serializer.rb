cfe8e7564ae027db9ec0616d4b649e023d3db39a
Autumn Kangas <autumnkangas@gmail.com>
2018-04-01 02:13:25 -0500

    wire up comments serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :book_id, :user_id, :content
+  attributes :id, :book_id, :user_id, :content, :updated_at
 
   belongs_to :user
   belongs_to :book
