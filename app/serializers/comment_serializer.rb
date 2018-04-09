5a8056082101a22d44e1d952918dc0723eab6b7c
Autumn Kangas <autumnkangas@gmail.com>
2018-04-09 11:49:36 -0500

    remove troubleshooting lines from serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :book_id, :user_id, :content, :updated_at
 
-  belongs_to :user, include: false
-  belongs_to :book, include: false
+  belongs_to :user
+  belongs_to :book
 end
